31319e8aaf9782199fa20eac8fe17ab6ad46990c
Bhumika1524 <51358927+Bhumika1524@users.noreply.github.com>
2021-03-04 21:48:25 +0530

    Program to print Size of Data Types

---
+++ SizeOfDataTypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    cout<<"Size of Char : "<<sizeof(char)<<" byte\n";
+    cout<<"Size of Int : "<<sizeof(int)<<" bytes\n";
+    cout<<"Size of Float : "<<sizeof(float)<<" bytes\n";
+    cout<<"Size of Double : "<<sizeof(double)<<" bytes";
+
+    return 0;
+}
